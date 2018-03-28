e4c6c1df117d1f7040974f56f0e145956da54044
malamsyah <mochammadalamsyah24@gmail.com>
2018-03-29 02:49:51 +0700

    First Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Mochammad Alamsyah on 29/03/18.
+//  Copyright © 2018 malamsyah.id. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
